11535a233f7d93aabc40855db269d8306ac27c71
ActuallyFlamey <flamey@hexa.blue>
2026-09-14 11:15:46 +0200

    fix: allow empty variable for SKU

--- utils/sku.py
+++ utils/sku.py
@@ -5,11 +5,12 @@ import dotenv
 
 dotenv.load_dotenv()
 
-USER_SUPPORTER_SKU_ID = int(os.getenv("USER_SUPPORTER_SKU_ID", "0"))
+user_supporter_env = os.getenv("USER_SUPPORTER_SKU_ID", "0")
+USER_SUPPORTER_SKU_ID = int(user_supporter_env if user_supporter_env != "" else 0)
 
 async def is_user_supporter(ctx: discord.Interaction) -> bool:
     for entitlement in ctx.entitlements:
         if entitlement.sku_id == USER_SUPPORTER_SKU_ID and not entitlement.deleted and not entitlement.is_expired():
             return True
-    
+
     return False
